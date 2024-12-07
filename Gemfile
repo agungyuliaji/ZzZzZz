38f4c975f2e993b663d2020b6a05f62cb79fcbcd
Handsomeboi <orang@ganteng.id>
2024-12-07 13:53:04 +0700

    initiate annotate models

--- Gemfile
+++ Gemfile
@@ -9,4 +9,5 @@ group :development, :test do
   gem "debug", platforms: %i[ mri windows ], require: "debug/prelude"
   gem "brakeman", require: false
   gem "rubocop-rails-omakase", require: false
+  gem "annotaterb"
 end
--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -1,3 +1,14 @@
+# == Schema Information
+#
+# Table name: users
+#
+#  id             :integer          not null, primary key
+#  clock_in_time  :datetime
+#  clock_out_time :datetime
+#  name           :string
+#  created_at     :datetime         not null
+#  updated_at     :datetime         not null
+#
 require "test_helper"
 
 class UserTest < ActiveSupport::TestCase
