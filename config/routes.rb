801d2fd63ead4d7c12eea6717d51486b2382f186
Handsomeboi <orang@ganteng.id>
2024-12-08 11:54:15 +0700

    generate sleep record operation skeleton

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 Rails.application.routes.draw do
+  namespace :sleep_records do
+    get "operation/sleep"
+    get "operation/wake_up"
+    get "operation/reset"
+  end
+
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
