e1fea278c05dfd9501e6adabea40fc6bae9fb12e
Handsomeboi <orang@ganteng.id>
2024-12-08 13:18:50 +0700

    run annotaterb

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,3 +1,14 @@
+# == Schema Information
+#
+# Table name: users
+#
+#  id             :integer          not null, primary key
+#  clock_in_time  :datetime
+#  clock_out_time :datetime
+#  name           :string
+#  created_at     :datetime         not null
+#  updated_at     :datetime         not null
+#
 FactoryBot.define do
   factory :user do
     name { "John Doe" }
--- spec/models/sleep_record_spec.rb
+++ spec/models/sleep_record_spec.rb
@@ -1,3 +1,23 @@
+# == Schema Information
+#
+# Table name: sleep_records
+#
+#  id             :integer          not null, primary key
+#  clock_in_time  :datetime
+#  clock_out_time :datetime
+#  duration       :integer
+#  created_at     :datetime         not null
+#  updated_at     :datetime         not null
+#  user_id        :integer          not null
+#
+# Indexes
+#
+#  index_sleep_records_on_user_id  (user_id)
+#
+# Foreign Keys
+#
+#  user_id  (user_id => users.id)
+#
 require 'rails_helper'
 
 RSpec.describe SleepRecord, type: :model do
